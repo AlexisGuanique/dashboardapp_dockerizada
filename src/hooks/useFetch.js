167a8d7a1f3399cc5e5ad6ae43d23763188a54f5
Alexis Guanique <guaniqued@gmail.com>
2022-11-29 17:52:57 -0300

    Agregar el setInterval

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -14,35 +14,35 @@ export const useFetch = (items) => {
     })
 
     const response = () => {
-        //! Agregar setInterval
-        //! setInterval(() => {
-        //! }, 120000);
-        
-        try {
-            Promise.all(items.map((item) => getService.get(item, {
-                validateStatus: function (status) {
-                    return status !== 500
-                }
-            })
-                .catch(error => {
-                    const findedElement = testData.find(element => element.config.url === error.config.url);
-
-                    if (!findedElement) {
-                        testData.push({ ...error })
+        setInterval(() => {
+            try {
+                Promise.all(items.map((item) => getService.get(item, {
+                    validateStatus: function (status) {
+                        return status !== 500
                     }
-                })))
+                })
+                    .catch(error => {
+                        const findedElement = testData.find(element => element.config.url === error.config.url);
 
-                .then(((res) => {
-                    setState({ ...state, data: res, isLoading: false })
-                }))
+                        if (!findedElement) {
+                            testData.push({ ...error })
+                        }
+                    })))
 
-        }
-        catch (error) {
-            setState({
-                ...state,
-                hasError: error
-            })
-        }
+                    .then(((res) => {
+                        setState({ ...state, data: res, isLoading: false })
+                    }))
+
+            }
+            catch (error) {
+                setState({
+                    ...state,
+                    hasError: error
+                })
+            }
+        }, 5000);
+
+
     }
 
     useEffect(() => {
@@ -51,7 +51,7 @@ export const useFetch = (items) => {
 
     }, []);
 
-  
+
     return {
         state: state,
         testData: testData,
